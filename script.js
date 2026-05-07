e29aa03bc99640fa586328fac39003c18d6e2cfe
mab-eric313 <kseneric313@gmail.com>
2026-05-07 20:58:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputValue = document.getElementById("inputTask");
+const btnTambah = document.getElementById("btnTambahTodo");
+const daftarTugas = document.getElementById("listTugas");
+
+btnTambah.addEventListener("click", function() {
+	if (inputValue.value === "") {
+		alert("Input tidak boleh kosong");
+		return;
+	}
+
+	const listBaru = document.createElement("li");
+	const span = document.createElement("span");
+	span.innerHTML = inputValue.value;
+
+	listBaru.appendChild(span);
+	daftarTugas.appendChild(listBaru);
+
+	inputValue.value = "";
+	inputValue.focus();
+});
