b74c3861e5778b6975f61f219f222254deceabcd
mab-eric313 <kseneric313@gmail.com>
2026-05-07 22:20:19 +0700

    tombol centang di Tasks memindahkan dari 'Tasks' ke 'Done'

--- script.js
+++ script.js
@@ -10,23 +10,44 @@ btnTambah.addEventListener("click", function() {
 		return;
 	}
 
-	const listBaru = document.createElement("li");
-	const span = document.createElement("span");
-	const newDiv = document.createElement("div");
-	const newButton = document.createElement("button")
-	const newButtonDelete = document.createElement("button")
+	const listBaruTask = document.createElement("li");
+	const spanTask = document.createElement("span");
+	const newDivTask = document.createElement("div");
+	const newButtonTask = document.createElement("button")
+	const newButtonDeleteTask = document.createElement("button")
 
-	span.innerHTML = inputValue.value;
-	newButton.innerHTML = "✔️";
-	newButtonDelete.innerHTML = "✖️";
+	spanTask.innerHTML = inputValue.value;
+	newButtonTask.innerHTML = "✔️";
+	newButtonDeleteTask.innerHTML = "✖️";
 
-	newDiv.appendChild(newButton);
-	newDiv.appendChild(newButtonDelete);
-	newDiv.appendChild(span)
+	newDivTask.appendChild(newButtonTask);
+	newDivTask.appendChild(newButtonDeleteTask);
+	newDivTask.appendChild(spanTask)
 
-	listBaru.appendChild(newDiv);
-	daftarTugas.appendChild(listBaru);
+	listBaruTask.appendChild(newDivTask);
+	daftarTugas.appendChild(listBaruTask);
 
 	inputValue.value = "";
 	inputValue.focus();
+	
+	newButtonTask.addEventListener("click", function() {
+		const listBaru = document.createElement("li");
+		const span = document.createElement("span");
+		const newDiv = document.createElement("div");
+		const newButton = document.createElement("button")
+		const newButtonDelete = document.createElement("button")
+
+		span.innerHTML = spanTask.innerHTML;
+		newButton.innerHTML = "✔️";
+		newButtonDelete.innerHTML = "✖️";
+
+		newDiv.appendChild(newButton);
+		newDiv.appendChild(newButtonDelete);
+		newDiv.appendChild(span)
+
+		listBaru.appendChild(newDiv);
+		daftarProgress.appendChild(listBaru);
+
+		listBaruTask.removeChild(newDivTask);
+	});
 });
